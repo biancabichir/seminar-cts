4ebdad1179e5c5b9c213c396d6a726a738858f71
Bianca.Bichir <Bianca.Bichir@DESKTOP-RIN24BQ>
2021-02-25 12:27:46 +0200

    final seminar 1

---
+++ sem1_cts/src/clase/Zookeeper.java
@@ -0,0 +1,13 @@
+package clase;
+
+public class Zookeeper {
+	private String name;
+	
+	public void feed(Animal animal) {
+		System.out.println(name+" hraneste animalul "+animal.getName());
+		
+	}
+	
+	
+
+}
--- sem1_cts/src/sem1_cts/Main.java
+++ sem1_cts/src/sem1_cts/Main.java
@@ -1,10 +1,30 @@
 package sem1_cts;
 
+import clase.Girafa;
+import clase.Zebra;
+import clase.Zoo;
+import clase.Zookeeper;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("hello");
+    Zoo zoo=new Zoo();
+    Zookeeper keeper=new Zookeeper();
+    
+    zoo.setKeeper1(keeper);
+    
+    Zebra zebra1=new Zebra("zebra1");
+    Zebra zebra2=new Zebra("zebra2");
+    zoo.add(zebra1);
+    zoo.add(zebra2);
+    zoo.feedAll();
+    Girafa girafa1=new Girafa("girafa1");
+    Girafa girafa2=new Girafa("girafa2");
+    zoo.add(girafa1);
+    zoo.add(girafa2);
+    zoo.feedAll();
 	}
 
 }
