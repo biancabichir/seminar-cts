d7ba34eee43ad84cc2f9b3583a0ef8c40300371c
Bianca.Bichir <Bianca.Bichir@DESKTOP-RIN24BQ>
2021-03-11 11:31:10 +0200

    seminar 3-final

--- sem2_cts/src/ro/ase/cts/clase/Aplicant.java
+++ sem2_cts/src/ro/ase/cts/clase/Aplicant.java
@@ -1,5 +1,7 @@
 package ro.ase.cts.clase;
 
+import java.util.Arrays;
+
 public abstract class Aplicant{
 	protected String nume;
 	protected String prenume;
@@ -69,5 +71,23 @@ public abstract class Aplicant{
 		this.nr_proiecte=nrProiecte;
 		this.denumireProiect=denumireProiect;
 	}
-
+	abstract public void afiseazaSumaFinantata() ;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Aplicant [nume=");
+		builder.append(nume);
+		builder.append(", prenume=");
+		builder.append(prenume);
+		builder.append(", varsta=");
+		builder.append(varsta);
+		builder.append(", punctaj=");
+		builder.append(punctaj);
+		builder.append(", nr_proiecte=");
+		builder.append(nr_proiecte);
+		builder.append(", denumireProiect=");
+		builder.append(Arrays.toString(denumireProiect));
+		return builder.toString();
+	}
+	
 }
