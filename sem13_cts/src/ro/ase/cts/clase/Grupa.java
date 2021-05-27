a57f46684bb3f36851fb1a9a303609b57c4f46fd
Bianca.Bichir <Bianca.Bichir@DESKTOP-RIN24BQ>
2021-05-27 11:23:38 +0300

    seminar 13-JUnitTest parte 2

--- sem13_cts/src/ro/ase/cts/clase/Grupa.java
+++ sem13_cts/src/ro/ase/cts/clase/Grupa.java
@@ -37,13 +37,15 @@ public class Grupa {
     }
 
     public float getPromovabilitate(){
+    	 if(studenti.size()==0)throw new IllegalArgumentException();
         int nrRestantieri=0;
         for(IStudent student:studenti){
             if(student.areRestante()){
                 nrRestantieri++;
             }
         }
-        return studenti.size()/nrRestantieri;
+       
+        return 1-((float)nrRestantieri/studenti.size());
     }
 
 	public int getNrGrupa() {
--- sem13_cts/src/ro/ase/cts/teste/TestGrupa.java
+++ sem13_cts/src/ro/ase/cts/teste/TestGrupa.java
@@ -2,12 +2,78 @@ package ro.ase.cts.teste;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
 
 public class TestGrupa {
-
+	private Grupa grupa;
+	
+	@Before
+	public void Setup() {
+		grupa=new Grupa(1077);
+		for(int i=0;i<10;i++) {
+			Student student=new Student("ALEX");
+			student.adaugaNota(5);
+			student.adaugaNota(7);
+			student.adaugaNota(4);
+			grupa.adaugaStudent(student);
+		}
+	}
+	
+	@Test
+	public void testGetPromovabilitate() {
+		Student studentA=new Student("Alex");
+		studentA.adaugaNota(10);
+		studentA.adaugaNota(10);
+		
+		Student studentM=new Student("MARIA");
+		studentM.adaugaNota(10);
+		studentM.adaugaNota(10);
+		
+		grupa.adaugaStudent(studentA);
+		grupa.adaugaStudent(studentM);
+		
+		double delta=0.05;
+		assertEquals(0.16, grupa.getPromovabilitate(),delta);
+		
+	}
+	
+	@Test
+	public void testGetPromovabilitateLBoundary() {
+		assertEquals(0, grupa.getPromovabilitate(),0.05);
+	}
+	
+	@Test(timeout=500)
+	public void testGetPromovabilitatePerformance() {
+		grupa.getPromovabilitate();
+	}
+	
+	@Test
+	public void testGetPromovabilitateUBoundary() {
+		Grupa grNoua=new Grupa(1076);
+		Student student=new Student("Ion");
+		student.adaugaNota(10);
+		student.adaugaNota(10);
+		
+		Student student1=new Student("Ioana");
+		student.adaugaNota(8);
+		student.adaugaNota(7);
+		
+		grNoua.adaugaStudent(student);
+		grNoua.adaugaStudent(student1);
+		assertEquals(1, grNoua.getPromovabilitate(),0.05);
+		
+	}
+	
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetPromovabilitateError() {
+    	Grupa grNoua=new Grupa(1080);
+    	grNoua.getPromovabilitate();
+    }
+    
 	@Test
 	public void testRightConstructor() {
 		Grupa grupa=new Grupa(1077);
